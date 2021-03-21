cb700c2d12e1b73e68890ef7af7cbbbb01a92922
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2021-03-21 18:17:40 -0300

    Working example and README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Steganography
+
+Simple steganography algorithm
+
+## Run working example
+
+1. `pip3 install -r requirements.txt`
+2. `python3 main.py`
+3. Open `resources/decoded_message.txt`. You should see `Sample text` as the first words.
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from steganography import Steganography
+from steganography.steganography import Steganography
 
 
 def main():
-    path = "/mnt/c/Users/Jefferson/Pictures/apyr.jpg"
+    path = "./resources/apyr.jpg"
     s = Steganography(path)
     
-    s.encode("Teste")
-    s.save("/mnt/c/Users/Jefferson/Pictures/apyr 2.jpg")
+    s.encode("Sample text")
+    s.save("./resources/apyr - encoded.jpg")
     
-    with open("/mnt/c/Users/Jefferson/Pictures/decoded_message.txt", "w") as file:
+    with open("./resources/decoded_message.txt", "w") as file:
         file.write(s.decode())
 
 
