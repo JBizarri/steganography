d2f2d75779f3a2e7ec51b723bcc19de117ffdee4
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2021-03-28 11:38:53 -0300

    Remove file creation from example

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def main():
     s.encode("Sample text")
     s.save("./resources/apyr - encoded.jpg")
 
-    with open("./resources/decoded_message.txt", "w") as file:
-        file.write(s.decode("./resources/apyr - encoded.png"))
+    print(s.decode("./resources/apyr - encoded.png"))
 
 
 if __name__ == "__main__":
