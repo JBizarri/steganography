78058428b03f13ca0e2e671df1f48dd02db2ab6e
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2021-03-28 10:17:53 -0300

    Fix error when encoding message and add end signal

--- steganography/steganography.py
+++ steganography/steganography.py
@@ -5,31 +5,35 @@ from PIL import Image
 
 
 class Steganography:
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, end: str = None) -> None:
         self._original_image = Image.open(path)
 
         self._encoded_image = None
+        self._end_message = end or r"\end"
 
     def encode(self, message: str) -> None:
+        """Encode image with a string message
+
+        Args:
+            message (str): message to encode
+        """
         image = self._original_image.copy()
         width, height = image.size
         pixels = image.load()
 
-        binary_string = self._str_to_binary_string(message)
-        tribit_list = re.findall("...", binary_string)
+        binary_string = self._str_to_binary_string(message + self._end_message)
+        tribit_list = self._split(binary_string, 3)
 
         for x_pixel in range(width):
             for y_pixel in range(height):
                 tribit = tribit_list.pop(0)
-                rgb = pixels[x_pixel, y_pixel]
-                red, green, blue = self._rgb_to_binary(rgb)
+                rgb_binary = self._rgb_to_binary(pixels[x_pixel, y_pixel])
+                new_pixel = self._write_to_lsb(tribit, rgb_binary)
 
-                red = red[:-1] + tribit[0]
-                green = green[:-1] + tribit[1]
-                blue = blue[:-1] + tribit[2]
+                if len(new_pixel) < 3:
+                    new_pixel = self._complete_new_pixel(new_pixel, rgb_binary)
 
-                rgb = self._binary_to_rgb(red, green, blue)
-                pixels[x_pixel, y_pixel] = rgb
+                pixels[x_pixel, y_pixel] = self._binary_to_rgb(*new_pixel)
 
                 if not tribit_list:
                     break
@@ -40,6 +44,14 @@ class Steganography:
         self._encoded_image = image
 
     def decode(self, path: str = None) -> str:
+        """Decode image and returns the hidden message
+
+        Args:
+            path (str, optional): Path of the image, if not specified uses the encoded image from self. Defaults to None.
+
+        Returns:
+            str: Decoded message
+        """
         if path:
             image = Image.open(path)
         else:
@@ -55,9 +67,14 @@ class Steganography:
                 red, blue, green = self._rgb_to_binary(rgb)
                 binary_string += red[-1] + blue[-1] + green[-1]
 
-        return self._binary_string_to_str(binary_string)
+        return self._binary_string_to_str(binary_string, end=self._end_message)
 
     def save(self, path: str) -> None:
+        """Save image as png.
+
+        Args:
+            path (str): path to save image.
+        """
         path = self._path_as_png(path)
 
         if self._encoded_image:
@@ -66,7 +83,77 @@ class Steganography:
             print("Error! Image was not encoded yet.")
 
     @staticmethod
-    def _path_as_png(filepath):
+    def _write_to_lsb(tribit: str, rgb_binary: tuple):
+        """Writes tribit to least significant bit from rgb_binary
+
+        Args:
+            tribit (str): A string with 3 bits
+            rgb_binary (tuple): RGB values as binary
+
+        Returns:
+            list: rgb_binary with least significant bits as tribit
+        """
+        pixel = []
+        for idx, bit in enumerate(tribit):
+            color = rgb_binary[idx]
+            color = color[:-1] + bit
+            pixel.append(color)
+
+        return pixel
+
+    @staticmethod
+    def _split(string: str, n: int):
+        """Split string every n characters.
+
+        Args:
+            string (str): String to be splitted.
+            n (int): group size.
+
+        Returns:
+            list: A list containing substrings of size n.
+        """
+        return [string[start : start + n] for start in range(0, len(string), n)]
+
+    @staticmethod
+    def _complete_new_pixel(new_pixel: list, old_pixel: tuple):
+        """Completes new_pixel with values from old_pixel
+
+        If new_pixel has 1 elements and old_pixel as 3, new_pixel will
+        use the second and third element from old_pixel so it also has
+        3 elements.
+
+        Args:
+            new_pixel (list): List with RGB values for a pixel
+            old_pixel (tuple): List with RGB values for a pixel
+
+        Returns:
+            list: New pixel with the same length as old_pixel using the
+            latter's values to complete any missing element.
+        """
+        if len(new_pixel) < len(old_pixel):
+            new_pixel_ = list(new_pixel).copy()
+
+            n_missing_elements = len(old_pixel) - len(new_pixel_)
+            start = -1
+            end = (n_missing_elements + 1) * -1
+            for i in range(start, end, -1):
+                new_pixel_.append(old_pixel[i])
+
+        return new_pixel_
+
+    @staticmethod
+    def _path_as_png(filepath: str):
+        """Returns filepath with PNG extension if it's a file.
+
+        Args:
+            filepath (str): Path to the file.
+
+        Raises:
+            IsADirectoryError: When filepath is a directory.
+
+        Returns:
+            str: filepath with png as extension.
+        """
         path = pathlib.Path(filepath)
         if path.is_dir():
             raise IsADirectoryError(path)
@@ -78,6 +165,17 @@ class Steganography:
 
     @staticmethod
     def _str_to_binary_string(string: str) -> str:
+        """Converts regular text to binary.
+
+        Args:
+            string (str): Text to be converted.
+
+        Raises:
+            ValueError: When return value is empty.
+
+        Returns:
+            str: Text as binary.
+        """
         binary_string = ""
         for char in string:
             ascii_code = ord(char)
@@ -89,17 +187,41 @@ class Steganography:
             raise ValueError("Error converting message to binary")
 
     @staticmethod
-    def _binary_string_to_str(binary_string: str) -> str:
+    def _binary_string_to_str(binary_string: str, end=None) -> str:
+        """Converts string with binary numbers to text string
+
+        Args:
+            binary_string (str): A string with binary numbers for ASCII
+            codes.
+            end (string, optional): Stops when end is found. Defaults to None.
+
+        Returns:
+            str: Text converted from binary string.
+        """
         string = ""
 
         binary_list = re.findall("." * 8, binary_string)
         for byte in binary_list:
             string += chr(int(byte, 2))
+            if end and string.endswith(end):
+                return string[: -len(end)]
 
         return string
 
     @staticmethod
     def _rgb_to_binary(rgb: tuple) -> tuple:
+        """Converts tuple of decimal string to binary
+
+        Args:
+            rgb (tuple): An RGB tuple. Example: (255, 255, 255).
+
+        Raises:
+            ValueError: When the tuple is not of length 3.
+
+        Returns:
+            tuple: Binary tuple.
+            Example ("11111111", "11111111", "11111111").
+        """
         if len(rgb) != 3:
             raise ValueError("RGB must be a tuple with 3 values")
 
@@ -113,6 +235,11 @@ class Steganography:
 
     @staticmethod
     def _binary_to_rgb(*args) -> tuple:
+        """Converts RGB binary values to decimal.
+
+        Returns:
+            tuple: RGB values in the decimal system.
+        """
         if len(args) == 1:
             red = args[0][0]
             green = args[0][1]
