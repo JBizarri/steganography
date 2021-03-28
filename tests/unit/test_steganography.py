e24db63de78713bfa5748414d1ea02678f4562e2
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2021-03-28 11:14:08 -0300

    Fix to black formatting

--- tests/unit/test_steganography.py
+++ tests/unit/test_steganography.py
@@ -17,7 +17,7 @@ def random_words(random):
         letters = string.ascii_lowercase
         length = random.randint(10, 80)
         return "".join(random.choice(letters) for _ in range(length))
-    
+
     return [random_word() for _ in range(10)]
 
 
@@ -25,7 +25,7 @@ def test_encode_and_decode_with_path_for_decoding(tmp_folder, test_image, random
     # ARRANGE
     for idx, word in enumerate(random_words):
         s = Steganography(test_image)
-        
+
         # ACT
         s.encode(word)
 
@@ -38,15 +38,16 @@ def test_encode_and_decode_with_path_for_decoding(tmp_folder, test_image, random
         assert result == word
 
 
-def test_encode_and_decode_without_path_for_decoding(tmp_folder, test_image, random_words):
+def test_encode_and_decode_without_path_for_decoding(
+    tmp_folder, test_image, random_words
+):
     # ARRANGE
     for word in random_words:
         s = Steganography(test_image)
-        
+
         # ACT
         s.encode(word)
         result = s.decode()
 
         # ASSERT
         assert result == word
-
